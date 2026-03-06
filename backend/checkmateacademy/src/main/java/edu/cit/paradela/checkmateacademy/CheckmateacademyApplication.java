4c83b3d5087b18f9d7b9e42da7d6bf224fb3dd2f
gwapoko123321 <paradelalawrence29@gmail.com>
2026-03-06 13:39:36 +0800

    Initialize project structure and Spring Boot backend foundation

---
+++ backend/checkmateacademy/src/main/java/edu/cit/paradela/checkmateacademy/CheckmateacademyApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.paradela.checkmateacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckmateacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckmateacademyApplication.class, args);
+	}
+
+}
---
+++ backend/checkmateacademy/src/test/java/edu/cit/paradela/checkmateacademy/CheckmateacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.paradela.checkmateacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckmateacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
